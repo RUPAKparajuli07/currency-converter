1118b4062266cb76edeba6b3c4b30db63a0fce05
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-10-26 13:43:03 +0545

    Update currency converter.py

--- currency converter/currency converter.py
+++ currency converter/currency converter.py
@@ -8,7 +8,7 @@ class App:
         self.from_currency = tk.StringVar(self.window)
         self.to_currency = tk.StringVar(self.window)
         self.amount = tk.StringVar(self.window)
-        self.converted_amount = tk.StringVar(self.window)
+        self.converted_amount = tk.StringVar(self.window) 
         self.url = url
 
         self.from_currency_label = tk.Label(self.window, text="From Currency")
