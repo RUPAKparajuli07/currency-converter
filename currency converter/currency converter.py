cd384f05384f760359593606c3629f556a564cbf
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-10 10:48:08 +0545

    Update currency converter.py

--- currency converter/currency converter.py
+++ currency converter/currency converter.py
@@ -1,3 +1,13 @@
+import tkinter as tk
+import requests
+
+class App:
+    def __init__(self, url):
+        self.window = tk.Tk()   
+        self.window.title("Currency Converter")
+        self.from_currency = tk.StringVar(self.window)
+        self.converted_amount = tk.StringVar(self.window) 
+        self.url = url
 
         self.from_currency_label = tk.Label(self.window, text="From Currency")
         self.from_currency_label.grid(column=0, row=0, padx=5, pady=5)
